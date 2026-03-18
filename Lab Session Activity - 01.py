f178cf8723fd01e5129aec44dcf18f48956fbb68
Sathsilu Gunawardana <ggysgunawardana@gmail.com>
2026-03-18 19:53:05 +0530

    Add files via upload

---
+++ Lab Session Activity - 01.py
@@ -0,0 +1,36 @@
+#Question 1
+print("Sathsilu Gunawardana")
+print("Ananda College")
+
+#Single Line Comment
+
+"""
+Multiline Comment
+This is the second line
+"""
+
+#to help other programmers understand the code by providing context, explanations, and documentation
+
+
+#Question 2
+result=22/7
+print("the value of 22/7 is", result)
+
+
+#Question 3
+"""
+What is meant by concatenation?
+• Concatenation means joining two or more strings together to form a single string. 
+"""
+
+operation = "sum"
+total = 8
+print(operation + " is " + str(total))
+
+
+#Question 4
+length = 10
+width = 5
+
+area = length * width
+print("The area of a rectangle with length " + str(length) + " and with " + str(width) + " is " + str(area))
