e8fbdde3bb50b2ff0c55f55b56bd04b3eaeb7b47
Sathsilu Gunawardana <ggysgunawardana@gmail.com>
2026-06-07 02:31:21 +0530

    Update Lab Activity - 2.py

--- Lab Activity - 2.py
+++ Lab Activity - 2.py
@@ -1,27 +1,21 @@
 #Question 1
 
-#What is a conditional statement in Python?
-
 #Conditional statements in Python are used to execute certain blocks of code based on specific conditions.
-
-#Explain if, elif and else
 
 #if - Runs code if the condition is True.
 #elif - Checks another condition if the previous ones were False.
 #else - Runs if all conditions above were False.
 
-#Python Program
-
 num1 = 10
 
 if num1 > 0:
-    print("Positive number")
+    print("\nPositive number")
     
 elif num1 == 0:
-    print("Zero")
+    print("\nZero")
     
 else:
-    print("Negative number")
+    print("\nNegative number")
 
    
 #Question 2
@@ -29,10 +23,10 @@ else:
 num1 = int(input("Enter a number: "))
 
 if num1 > 0:
-    print("Postive number")
+    print("\nPostive number")
     
 else:
-    print("Negative number")
+    print("\nNegative number")
 
 
 #Question 3
@@ -42,10 +36,10 @@ num1 = int(input("Enter a number: "))
 remainder = num1 % 2
 
 if remainder == 0:
-    print("The number is even")
+    print("\nThe number is even")
     
 else:
-    print("The number is odd")
+    print("\nThe number is odd")
 
 
 #Question 4
@@ -53,16 +47,17 @@ else:
 age = int(input("Enter your age: "))
 
 if age >= 18:
-    print("You are eligible to vote")
+    print("\nYou are eligible to vote")
 
 else:
-    print("You are not eligible to vote")
+    print("\nYou are not eligible to vote")
 
 
 #Question 5
     
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+print()
 
 if num1 > num2:
     print(num1, "is larger than", num2)
@@ -78,13 +73,13 @@ num2 = int(input("Enter the second number: "))
 num3 = int(input("Enter the third number: "))
 
 if num1 > num2 and num1 > num3:
-    print("The largest number is: ", num1)
+    print("\nThe largest number is: ", num1)
     
 elif num2 > num1 and num2 > num3:
-    print("The largest number is: ", num2)
+    print("\nThe largest number is: ", num2)
     
 else:
-    print("The largest number is: ", num3)
+    print("\nThe largest number is: ", num3)
 
     
 #Question 7
@@ -92,13 +87,13 @@ else:
 num1 = int(input("Enter a number: "))
 
 if num1 > 0:
-    print("Positive number")
+    print("\nPositive number")
     
 elif num1 == 0:
-    print("Zero")
+    print("\nZero")
     
 else:
-    print("Negative number")
+    print("\nNegative number")
 
 
 #Question 8
@@ -106,16 +101,16 @@ else:
 marks = int(input("Enter your marks: "))
 
 if marks >= 75:
-    print("Your grade is A")
+    print("\nYour grade is A")
     
 elif marks >= 60:
-    print("Your grade is B")
+    print("\nYour grade is B")
     
 elif marks >= 50:
-    print("Your grade is C")
+    print("\nYour grade is C")
     
 else:
-    print("Your grade is F")
+    print("\nYour grade is F")
 
     
 #Question 9
@@ -124,10 +119,10 @@ username = str(input("Enter your username: "))
 password = str(input("Enter your password: "))
 
 if username == "admin" and password == "1234":
-    print("Login successful")
+    print("\nLogin successful")
     
 else:
-    print("Login unsuccessful")
+    print("\nLogin unsuccessful")
 
 
 #Question 10
@@ -136,15 +131,15 @@ price = int(input("Enter purchasing price: "))
 
 if price > 5000:
     discounted = price - (price * 20 / 100)
-    print("Discounted price = ", discounted)
+    print("\nDiscounted price = ", discounted)
     
 elif price >= 2000:
     discounted = price - (price * 10 / 100)
-    print("Discounted price = ", discounted)
+    print("\nDiscounted price = ", discounted)
     
 else:
     discounted = price
-    print("You get no discount")
+    print("\nYou get no discount")
 
 
 #Bonus Question
@@ -160,4 +155,4 @@ elif units <= 200:
 else:
     bill = (100 * 10) + (100 * 15) + (units - 200) * 20
 
-print("Your bill is: ", bill)
+print("\nYour bill is: ", bill)
