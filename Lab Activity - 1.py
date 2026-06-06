087032b770d70bcbc9af5d2b28fdfbe4c6a88ca4
Sathsilu Gunawardana <ggysgunawardana@gmail.com>
2026-06-07 02:22:00 +0530

    Refactor Lab Activity - 1.py with additional questions

--- Lab Activity - 1.py
+++ Lab Activity - 1.py
@@ -3,6 +3,7 @@
 print("Sathsilu Gunawardana")
 print("Ananda College")
 
+
 #Single Line Comment
 
 """
@@ -10,7 +11,7 @@ Multiline Comment
 This is the second line
 """
 
-#to help other programmers understand the code by providing context, explanations, and documentation
+#To help other programmers understand the code by providing context, explanations, and documentation
 
 
 #Question 2
@@ -23,7 +24,7 @@ print("the value of 22/7 is", result)
 
 """
 What is meant by concatenation?
-• Concatenation means joining two or more strings together to form a single string. 
+Concatenation means joining two or more strings together to form a single string. 
 """
 
 operation = "sum"
@@ -40,3 +41,44 @@ width = 5
 area = length * width
 
 print("The area of a rectangle with length " + str(length) + " and with " + str(width) + " is " + str(area))
+
+
+#Question 5
+
+name = input("Enter your name: ")
+
+print("Hello " + name + ", welcome to NSBM!")
+print(type(name))
+
+
+#Question 6
+
+birth_year = input("Enter your birth year: ")
+
+birth_year = int(birth_year)
+
+print("Your birth year is:", birth_year)
+print(type(birth_year))
+
+#Typecasting is the process of converting a value from one data type to another.
+
+
+#Question 7
+
+birth_year = int(input("Enter your birth year: "))
+
+age = 2025 - birth_year
+
+print("Your age is:", age)
+
+
+#Bonus Question
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+print("\nAddition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Power:", num1 ** num2)
